4f58af1847ddc0f007966d5edc7383f125b4d015
Mo-Sanchez <moisessanchez6100@gmail.com>
2025-09-25 15:01:59 -0400

    Features: Admin page password authentication

--- obj/Debug/net9.0/EscalationMatrixCountdown.AssemblyInfo.cs
+++ obj/Debug/net9.0/EscalationMatrixCountdown.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EscalationMatrixCountdown")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48a83012e6a0eeb65f3a5abd45aab50d7724e3fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3030d086badc88ee4d5be1b2742869525c36d7f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EscalationMatrixCountdown")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EscalationMatrixCountdown")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
